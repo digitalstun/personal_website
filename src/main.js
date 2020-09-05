7583c74e242831cb151920db4e3e89cb180a3411
Digitalstun-D <digitalstun@gmail.com>
2020-09-04 21:13:20 -0400

    added .cache folder to gitignore

--- src/main.js
+++ src/main.js
@@ -5,21 +5,19 @@ const app = express();
 
 // Middleware 
 app.use("/src", express.static("src"));
-
 // navigation routes 
 // index page 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./index.html"));
 });
 // contact page
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "src", "/contact.html"));
-// });
+ app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./contact.html"));
+ });
 // catch 404 and forward error handler
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "./404.html"));
 });
-
 // server
 app.listen(3000, function(){
     // create variable to hold date 
